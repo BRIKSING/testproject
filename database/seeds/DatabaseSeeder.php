54119c8bd9ae644f0147fcb776a73bc0ce327a79
BRIKSING <BRIKSING@yandex.ru>
2018-04-23 22:49:02 +0500

    Seeders for databae

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('periods')->insert([
+          ['name'=>'year'],
+          ['name'=>'month'],
+          ['name'=>'week'],
+          ['name'=>'day'],
+          ['name'=>'simple']
+        ]);
     }
 }
